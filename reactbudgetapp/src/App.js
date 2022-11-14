7fe8ab84277a6f6ef56f89f4604832d8771db3d4
lensonfernandes <lenson.fernand@gmail.com>
2022-11-14 15:48:24 +0530

    Map function added to create BudgetCards

--- reactbudgetapp/src/App.js
+++ reactbudgetapp/src/App.js
@@ -3,10 +3,14 @@ import {Button, Stack} from 'react-bootstrap';
 import BudgetCard from './components/BudgetCard';
 import AddBudgetModal from './components/AddBudgetModal';
 import { useState } from 'react';
+import { BudgetsProvider } from './Contexts/BudgetsContext';
+import {useBudgets} from './Contexts/BudgetsContext'
 
 
 function App() {
   const [showAddBudgetModal, setShowAddBudgetModal] = useState(false)
+  const {budgets, getBudgetExpenses} = useBudgets()
+
   return (
     <>
    <Container className="my-4"> 
@@ -17,6 +21,18 @@ function App() {
       </Stack>
 
      <div style={{display:"grid",gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))", gap:"1rem" , alignItems: "flex-start"}}>
+      {budgets.map((budget)=>{ 
+          const amount = getBudgetExpenses(budget.id).reduce((total, expense) => total + expense.amount, 0)
+        return (
+      
+        <BudgetCard 
+        key={budget.id}
+        name={budget.name}
+        amount={amount}
+        max={budget.max}
+        
+        />)
+      })}
         <BudgetCard name="Entertainment" amount={1600} max={1000} gray></BudgetCard>
      </div>
    </Container>
