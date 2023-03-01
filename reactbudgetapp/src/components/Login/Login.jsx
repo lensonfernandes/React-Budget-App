8b5ec39ae2a8f2b0ee9ddb6a3fdef2518834605f
Lenson Merwin Fernandes <lenson.fernand@gmail.com>
2023-03-01 18:21:21 +0530

    Login component Password and Button added

--- reactbudgetapp/src/components/Login/Login.jsx
+++ reactbudgetapp/src/components/Login/Login.jsx
@@ -6,6 +6,7 @@ import {
   CardFooter,
   Text,
   CardHeader,
+  Button
 } from "@chakra-ui/react";
 import { Input } from "@chakra-ui/react";
 
@@ -14,11 +15,26 @@ const Login = () => {
   const [password, setPassword] = useState("");
 
   const handleChange = (e) => {
-    setEmail(e.target.value);
+ const {name, value}= e.target;
+
+ (name=="email")? setEmail(value):setPassword(value)
+
+   
   };
 
+  let divStyle = {
+
+    backgroundColor:"grey",
+    padding:"20px",
+    height: "100vh",
+    display: "flex",
+    alignItems: "center",
+    width:"100%",
+    justifyContent:"center"
+  }
+
   return (
-    <div>
+    <div style={divStyle}>
       <Card>
         <CardHeader>
           <Heading size="md">Login</Heading>
@@ -29,11 +45,23 @@ const Login = () => {
             <Text mb="8px">Email:</Text>
             <Input
               value={email}
+              name="email"
               onChange={handleChange}
               placeholder="Enter Email"
               size="md"
             />
           </>
+          <>
+            <Text mb="8px">Password:</Text>
+            <Input
+              value={password}
+              onChange={handleChange}
+              placeholder="Enter Password"
+              name="password"
+              size="md"
+            />
+          </>
+          <Button colorScheme='blue' m={3}>Login</Button>
         </CardBody>
       </Card>
     </div>
